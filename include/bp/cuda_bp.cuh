a9dadc333b8c61aeabe8428fa562394df5b79a77
Antonio Mallia <me@antoniomallia.it>
2018-11-02 14:14:18 -0400

    Added cuda version

---
+++ include/bp/cuda_bp.cuh
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2018-present Antonio Mallia <me@antoniomallia.it>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <utility>
+
+namespace bp {
+
+namespace details {
+
+} // namespace details
+
+static void encode(uint8_t *out, const uint32_t *in, size_t n) {
+}
+
+static void decode(uint32_t *out, const uint8_t *in, size_t n) {
+}
+
+} // namespace bp
