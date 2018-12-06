b65f5e7e194eaf17de3faaa3adc1cc6bbe911a18
Antonio Mallia <me@antoniomallia.it>
2018-12-06 18:59:10 -0500

    Fix delta

--- include/bp/utils.hpp
+++ include/bp/utils.hpp
@@ -3,9 +3,9 @@
 namespace utils {
 
 static void delta_encode(uint32_t *in, size_t n) {
-  for (size_t i = 1; i < n; ++i) {
-    in[i] -= in[i-1];
-  }
+    for (size_t i = n - 1; i > 0; --i) {
+      in[i] -= in[i - 1];
+    }
 }
 
 }
